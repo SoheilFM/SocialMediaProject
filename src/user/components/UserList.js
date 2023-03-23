9e0591d8ce9ae9acbde5a6207881804637d54034
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-23 22:55:47 +0000

    Adding a UserList Page Component

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,4 +1,27 @@
 import React from 'react';
+import UserItem from './UserItem.js';
 import './UserList.css';
-const UserList = (props) => {};
+
+const UserList = (props) => {
+    if (props.item.length === 0) {
+        return (
+            <div className='center'>
+                <h2> No Users Found</h2>
+            </div>
+        );
+    }
+    return (
+        <ul>
+            {props.item.map((user) => (
+                <UserItem
+                    key={user.id}
+                    id={user.id}
+                    image={user.image}
+                    name={user.name}
+                    placeCount={user.place}
+                />
+            ))}
+        </ul>
+    );
+};
 export default UserList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import UserList from '../components/UserList';
 const Users = () => {
-    return <h2>Users Works!</h2>;
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Soheil',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUnTTZNCUh3VqBAL_8kLS7w5skv9NwxlrJyA&usqp=CAU',
+            place: 3,
+        },
+    ];
+    return <UserList item={USERS} />;
 };
 
 export default Users;
