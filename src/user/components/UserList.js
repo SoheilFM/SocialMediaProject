2d5bd7782f67778fb6ce1898830859ad43517c97
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-24 02:53:34 +0000

    Update UserList Path

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import UserItem from './UserItem.js';
-import Card from '../../shared/UIElements/Card.js';
+import Card from '../../shared/components/UIElements/Card.js';
 import './UserList.css';
 
 const UserList = (props) => {
