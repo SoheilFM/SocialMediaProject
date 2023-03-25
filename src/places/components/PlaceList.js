501a0faaabb2f802fffbbc320b7e3c5875568a85
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-25 01:57:08 +0000

    Adding Route Params and Fix PlaceList typo Error

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -3,7 +3,7 @@ import Card from '../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem';
 import './PlaceList.css';
 const PlaceList = (props) => {
-    if (props.items.lenght === 0) {
+    if (props.items.length === 0) {
         return (
             <div className='place-list center'>
                 <Card>
