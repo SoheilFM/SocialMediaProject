9807315504d76c26993cd90c17cfcea0d4fd10dd
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-25 01:41:38 +0000

    Adding UserPlaces  Data and Router

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description:
+            'One Of The Most Famous Sky Scrapers in the World!',
+        imageUrl:
+            'https://lh3.googleusercontent.com/p/AF1QipNVlM5lo7fIJrmvjN4EOrTMiQjDgDyTfw7ATdV6=s680-w680-h510',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584,
+        },
+        creator: 'u1',
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description:
+            'One Of The Most Famous Sky Scrapers in the World!',
+        imageUrl:
+            'https://lh3.googleusercontent.com/p/AF1QipNVlM5lo7fIJrmvjN4EOrTMiQjDgDyTfw7ATdV6=s680-w680-h510',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584,
+        },
+        creator: 'u2',
+    },
+];
+const UserPlaces = () => {
+    return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
