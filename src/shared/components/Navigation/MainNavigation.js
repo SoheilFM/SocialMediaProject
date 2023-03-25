a7026579ba22d985cf05fe5e130163bf52f5a1aa
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-25 00:02:02 +0000

    Add Drawer&BackDrop & Add Animation to SlideDrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader.js';
 import NavLinks from './NavLinks.js';
 import SideDrawer from './SideDrawer.js';
+import Backdrop from '../UIElements/Backdrop.js';
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    };
+
     return (
         <React.Fragment>
-            <SideDrawer>
+            {drawerIsOpen && (
+                <Backdrop onClick={closeDrawerHandler} />
+            )}
+            <SideDrawer
+                show={drawerIsOpen}
+                onClick={closeDrawerHandler}
+            >
                 <nav className='main-navigation__drawer-nav'>
                     <NavLinks />
                 </nav>
             </SideDrawer>
+
             <MainHeader>
-                <button className='main-navigation__menu-btn'>
+                <button
+                    className='main-navigation__menu-btn'
+                    onClick={openDrawerHandler}
+                >
                     <span />
                     <span />
                     <span />
