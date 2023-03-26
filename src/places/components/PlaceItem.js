17847003ac6ef3f4b614be80f517b2795243f339
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-26 02:08:13 +0000

    Adding Deletion Warning

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -7,11 +7,21 @@ import './PlaceItem.css';
 
 const PlaceItem = (props) => {
     const [showMap, setShowMap] = useState(false);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
 
     const openMapHandler = () => setShowMap(true);
 
     const closeMapHandler = () => setShowMap(false);
-
+    const showDeleteWarningHandler = () => {
+        setShowConfirmModal(true);
+    };
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false);
+    };
+    const confirmDeleteHandler = () => {
+        setShowConfirmModal(false);
+        console.log('DELETING...');
+    };
     return (
         <React.Fragment>
             <Modal
@@ -27,6 +37,27 @@ const PlaceItem = (props) => {
                 <div className='map-container'>
                     <h2>THE MAP!</h2>
                 </div>
+            </Modal>
+            <Modal
+                show={showConfirmModal}
+                onCancel={cancelDeleteHandler}
+                header='Are You Sure?'
+                footerClass='place-item__modal-actions'
+                footer={
+                    <React.Fragment>
+                        <Button inverse onClick={cancelDeleteHandler}>
+                            CANCEL
+                        </Button>
+                        <Button danger onClick={confirmDeleteHandler}>
+                            DELETE
+                        </Button>
+                    </React.Fragment>
+                }
+            >
+                <p>
+                    Do You Want To Proceed Delete This Place? Please
+                    Note That It Can't Be Undo thereafter.
+                </p>
             </Modal>
             <li className='place-item'>
                 <Card className='place-item__content'>
@@ -45,7 +76,12 @@ const PlaceItem = (props) => {
                         <Button to={`/places/${props.id}`}>
                             EDIT
                         </Button>
-                        <Button danger>DELETE</Button>
+                        <Button
+                            danger
+                            onClick={showDeleteWarningHandler}
+                        >
+                            DELETE
+                        </Button>
                     </div>
                 </Card>
             </li>
