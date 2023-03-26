683c4a4fafe302a42ba42575ae07e185b8998d4b
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-26 22:27:03 +0000

    Adding Post Route

--- backEnd/controllers/places-controllers.js
+++ backEnd/controllers/places-controllers.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid/v4');
 const HttpError = require('../models/http-error');
 const DUMMY_PLACES = [
     {
@@ -44,6 +45,21 @@ const getPlaceByUserId = (req, res, next) => {
     }
     res.json({ place });
 };
+const createPlace = (req, res, next) => {
+    const { title, description, coordinates, address, creator } =
+        req.body;
+    const createPlace = {
+        id: uuid(),
+        title,
+        description,
+        location: coordinates,
+        address,
+        creator,
+    };
+    DUMMY_PLACES.push(this.createPlace);
+    res.status(201).json({ place: createPlace });
+};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
