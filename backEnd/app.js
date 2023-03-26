f2da2d8fa652b3903d42eb65596b60abc12f6057
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-26 19:47:59 +0000

    Adding Error Handeling

--- backEnd/app.js
+++ backEnd/app.js
@@ -8,7 +8,14 @@ const port = 5000;
 
 app.use(bodyParser.json());
 app.use('/api/places', placesRoutes);
-
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500).json({
+        message: error.message || 'An Unknown Error Occurred',
+    });
+});
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
