ff7a7729d4a3b2ab1420e0951cd868e1911d99b5
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-26 01:41:25 +0000

    Adjusting the Form Hook  and Fix Minor Issues

--- src/shared/hooks/form-hook.js
+++ src/shared/hooks/form-hook.js
@@ -23,6 +23,11 @@ const formReducer = (state, action) => {
                 },
                 isValid: formIsValid,
             };
+        case 'SET_DATA':
+            return {
+                inputs: action.inputs,
+                isValid: action.formIsValid,
+            };
         default:
             return state;
     }
@@ -42,6 +47,12 @@ export const useForm = (initialInputs, initialFormValidity) => {
             inputId: id,
         });
     }, []);
-
-    return [formState, inputHandler];
+    const setFormData = useCallback((inputData, formValidity) => {
+        dispatch({
+            type: 'SET_DATA',
+            inputs: inputData,
+            formIsValid: formValidity,
+        });
+    }, []);
+    return [formState, inputHandler, setFormData];
 };
