bff8b8d3783f797ba511ecc510be930cfd534976
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-27 19:59:26 +0000

    getting Places By The UserId

--- backEnd/controllers/places-controllers.js
+++ backEnd/controllers/places-controllers.js
@@ -18,26 +18,38 @@ let DUMMY_PLACES = [
     },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
     const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find((p) => {
-        return p.id === placeId;
-    });
+    let place;
+    try {
+        place = await Place.findById(placeId);
+    } catch (err) {
+        const error = new HttpError(
+            'Something went Wrong, Could Not Find a Place',
+            500
+        );
+        return next(error);
+    }
     if (!place) {
-        throw new HttpError(
+        const error = new HttpError(
             'Could Not Find A Place For The Provided ID',
             404
         );
+        return next(error);
     }
 
-    res.json({ place });
+    res.json({ place: place.toObject({ getters: true }) });
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
     const userId = req.params.uid;
-    const places = DUMMY_PLACES.filter((p) => {
-        return p.creator === userId;
-    });
+    let places;
+    try {
+        places = await Place.find({ creator: userId });
+    } catch (err) {
+        const error = new HttpError('Fetching Places Failed', 500);
+        return next(error);
+    }
     if (!places || places.length === 0) {
         return next(
             new HttpError(
@@ -46,7 +58,11 @@ const getPlacesByUserId = (req, res, next) => {
             )
         );
     }
-    res.json({ places });
+    res.json({
+        places: places.map((place) =>
+            place.toObject({ getters: true })
+        ),
+    });
 };
 const createPlace = async (req, res, next) => {
     const errors = validationResult(req);
