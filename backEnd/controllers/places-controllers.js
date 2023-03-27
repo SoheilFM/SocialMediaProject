30633a16e91f898711c0c670b71649a5d6c0efe6
SoheilFM <soheil.farid1999@yahoo.com>
2023-03-27 00:21:14 +0000

    Fix Get Places Bug

--- backEnd/controllers/places-controllers.js
+++ backEnd/controllers/places-controllers.js
@@ -30,20 +30,20 @@ const getPlaceById = (req, res, next) => {
     res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
     const userId = req.params.uid;
-    const place = DUMMY_PLACES.find((p) => {
+    const places = DUMMY_PLACES.filter((p) => {
         return p.creator === userId;
     });
-    if (!place) {
+    if (!places || places.length === 0) {
         return next(
             new HttpError(
-                'Could Not Find A Place For The Provided User ID',
+                'Could Not Find Places For The Provided User ID',
                 404
             )
         );
     }
-    res.json({ place });
+    res.json({ places });
 };
 const createPlace = (req, res, next) => {
     const { title, description, coordinates, address, creator } =
@@ -81,7 +81,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
